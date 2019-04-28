c468fe5d2d77b05ff7b7e3973d8590abbcdead04
rodreego <rodrigogarcia.vix@gmail.com>
2019-04-28 01:53:59 -0300

    caraio

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -7,6 +7,8 @@ class DropBoxController{
 
         this.currentFolder = ['hcode'];
         this.onSelectionChange = new Event('selectionChange');
+
+        this.navEl = document.querySelector('#browse-location');
         this.btnSendFileEl = document.querySelector('#btn-send-file');
         this.inputFilesEl = document.querySelector('#files');
         this.snackModalEl = document.querySelector('#react-snackbar-root');
@@ -20,7 +22,8 @@ class DropBoxController{
         
         this.connectFireBase();
         this.initEvents();
-        this.readFiles();
+        this.openFolder();
+        
                 
     }
 
@@ -190,9 +193,11 @@ class DropBoxController{
         this.btnSendFileEl.disabled = false;
     }
 
-    getFirebaseRef(){
+    getFirebaseRef(path){
 
-        return firebase.database().ref('files');
+        if(!path) path = this.currentFolder.join('/');
+
+        return firebase.database().ref(path);
     }
 
     modalShow(show = true){
@@ -537,6 +542,8 @@ class DropBoxController{
 
     readFiles(){
 
+        this.lastFolder = this.currentFolder.join('/');
+
         this.getFirebaseRef().on('value', snapshot =>{
 
             this.listFilesEl.innerHTML = ' ';
@@ -544,20 +551,103 @@ class DropBoxController{
             snapshot.forEach(snapshotItem=>{
 
                 let key = snapshotItem.key;
-                let data = snapshotItem.val();          
+                let data = snapshotItem.val();    
+                
+                if (data.type){                
 
-                this.listFilesEl.appendChild(this.getFileView(data,key));
+                    this.listFilesEl.appendChild(this.getFileView(data,key));
+
+                }
 
             });                
             
         });
     }
 
+    openFolder(){
+
+        if(this.lastFolder) this.getFirebaseRef(this.lastFolder).off('value');
+
+        this.renderNav();
+        this.readFiles();
+
+    }
+
+    renderNav(){
+
+        let nav = document.createElement('nav');
+
+        let path = [];
+
+        for(let i = 0; i < this.currentFolder.length; i++){
+            
+            let folderName = this.currentFolder[i];
+
+            let span = document.createElement('span');
+
+            path.push(folderName);
+
+            if((i+1) === this.currentFolder.length){
+
+                span.innerHTML = folderName;
+
+            }else{
+                span.className = 'breadcrumb-segment_wrapper'
+                span.innerHTML = `
+                    <span class="ue-effect-container uee-BreadCrumbSegment-link-0">
+                        <a href="#" data-path=${path.join('/')} class="breadcrumb-segment">${folderName}</a>
+                    </span>
+                    <svg width="24" height="24" viewBox="0 0 24 24" class="mc-icon-template-stateless" style="top: 4px; position: relative;">
+                        <title>arrow-right</title>
+                        <path d="M10.414 7.05l4.95 4.95-4.95 4.95L9 15.534 12.536 12 9 8.464z" fill="#637282"
+                            fill-rule="evenodd"></path>
+                    </svg>
+                `;
+                
+            }
+            nav.appendChild(span);
+        }
+
+        this.navEl.innerHTML = nav.innerHTML;
+
+        this.navEl.querySelectorAll('a').forEach(a =>{
+
+            a.addEventListener('click',e=>{
+
+                e.preventDefault();
+
+                this.currentFolder = a.dataset.path.split('/');
+
+                this.openFolder();
+
+            });
+        });
+    }
+
+
     initEventsLi(li){
 
-        li.addEventListener('click', e =>{
+        li.addEventListener('dblclick',e=>{
 
+            let file = JSON.parse(li.dataset.file);
             
+            switch(file.type){
+
+                case 'folder':
+                    this.currentFolder.push(file.name);
+                    this.openFolder();                    
+                    break;
+
+                default:
+                    window.open('/file?path= '+file.path);
+    
+            }
+
+        });
+
+
+
+        li.addEventListener('click', e =>{           
 
 
             if(e.shiftKey){
